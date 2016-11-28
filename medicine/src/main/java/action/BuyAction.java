061a7906913a652f363995677cb08aa0452ffe6a
maokailu <1113901189@qq.com>
2016-11-28 16:07:00 +0800

    药品入库前台显示药品信息
    实现从后台返回数据显示在前台

--- medicine/src/main/java/action/BuyAction.java
+++ medicine/src/main/java/action/BuyAction.java
@@ -1,28 +1,60 @@
 package action;
 
-import java.util.Date;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
 
 import dao.BuyDao;
 import dao.BuyDaoJDBCImpl;
+import entity.Medicines;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import service.BuyService;
 import service.impl.BuyServiceImpl;
 
-public class BuyAction {
+public class BuyAction extends ActionSupport{
+	private String data;
 	//根据选择日期查询该日期所有入库药品
-	public String show(){
-		//调用Service层的代码
-		BuyService buyService=new BuyServiceImpl();
-		BuyDao buyDao=new BuyDaoJDBCImpl();
-		buyService.setBuyDao(buyDao);
-		Date date=new Date();
-		buyService.getMedicines(date.toString());
-		return null;
+	//输出指定字段
+	public PrintWriter out()throws IOException{
+		HttpServletResponse response=ServletActionContext.getResponse();  
+        response.setContentType("text/html");  
+        response.setContentType("text/plain; charset=utf-8");
+        PrintWriter out= response.getWriter();
+        return out;
 	}
-	public static void main(String[] args) {
+	public String show() throws IOException{
+		out();
+		//调用Service层代码
 		BuyService buyService=new BuyServiceImpl();
 		BuyDao buyDao=new BuyDaoJDBCImpl();
 		buyService.setBuyDao(buyDao);
-		Date date=new Date();
-		buyService.getMedicines(date.toString());
+		//根据所选择的日期查询当天入库的药品信息
+		List<Medicines> medicines = buyService.getMedicines("2016-11-08");
+		//将查出的数据返回前台
+		JSONArray array=new JSONArray();
+		for(Medicines m:medicines){
+			 JSONObject jo=new JSONObject();
+			 //字符型
+			 jo.put("id", m.getCategory_id());
+			 array.add(jo);
+		}
+		String str="{\"rows\":"+array+"}";
+		out().print(str);
+		out().flush(); 
+		out().close();
+		return SUCCESS;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
 	}
 }
